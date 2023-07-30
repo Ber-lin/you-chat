1c52fa33ba1c6c670acac487f18eac944c7e0988
BerLin_bl <1529366751@qq.com>
2023-07-30 15:57:42 +0800

    feat: app总体结构

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,97 @@
-import React from 'react';
+import React from "react";
+import { Breadcrumb, Layout, Menu, MenuProps, theme } from "antd";
+import { MailOutlined } from "@ant-design/icons";
+import "./index.less";
+import { Outlet, useNavigate } from "react-router-dom";
+import { routesArr } from "@/routes/index";
 
-function Home() {
-  return (
-    <div>
-      Home
-    </div>
-  );
+const { Header, Content, Footer } = Layout;
+
+type MenuItem = Required<MenuProps>["items"][number];
+function getItem(
+  label: React.ReactNode,
+  key: React.Key,
+  icon?: React.ReactNode,
+  children?: MenuItem[],
+  type?: "group"
+): MenuItem {
+  return {
+    key,
+    icon,
+    children,
+    label,
+    type,
+  } as MenuItem;
 }
+
+const items: MenuProps["items"] = [
+  getItem("Navigation One", "/", <MailOutlined />),
+
+  getItem("chat", "/chat", <MailOutlined />),
+
+  { type: "divider" },
+
+  getItem("login", "/login", <MailOutlined />),
+
+  getItem("no", "/sub4", <MailOutlined />),
+];
+
+const Home: React.FC = () => {
+  const navigate = useNavigate();
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+
+  const onClick: MenuProps["onClick"] = ({ key }) => {
+    navigate(key);
+  };
+  return (
+    <Layout className="layout">
+      <Header style={{ display: "flex", alignItems: "center" }}>
+        <div className="demo-logo" />
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={["2"]}
+          items={items}
+          onClick={onClick}
+        />
+      </Header>
+      <Content style={{ padding: "0 50px" }}>
+        <Breadcrumb
+          style={{ margin: "16px 0" }}
+          items={[
+            {
+              title: "Home",
+            },
+            {
+              title: <a href="">Application Center</a>,
+            },
+            {
+              title: <a href="">Application List</a>,
+            },
+            {
+              title: "An Application",
+            },
+          ]}
+        ></Breadcrumb>
+        <div
+          className="site-layout-content"
+          style={{
+            background: colorBgContainer,
+            margin: "24px 16px",
+            padding: 24,
+            minHeight: 280,
+          }}
+        >
+          <Outlet></Outlet>
+        </div>
+      </Content>
+      <Footer style={{ textAlign: "center" }}>
+        You-Chat ©2023 Created by 伏翎
+      </Footer>
+    </Layout>
+  );
+};
 
 export default Home;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,30 +4,35 @@ import NotFound from "@/pages/404";
 import Login from "@/pages/Login";
 import Home from "@/pages/Home";
 import ErrorBoundary from "@/components/ErrorBoundary";
+import Chat from "@/pages/Chat";
 
-
-
+export const routesArr: RouteObject[] = [
+  {
+    path: "/",
+    element: <Home />,
+    children:[
+      {
+        path: "/chat",
+        element: <Chat />,
+      },
+    ]
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "*",
+    element: <NotFound />,
+  },
+];
 export default function Route() {
-  const routesArr: RouteObject[] = [
-    {
-      path: "/",
-      element: <Home />,
-
-    },
-
-    {
-      path: "/login",
-      element: <Login />,
-    },
-    {
-      path: "*",
-      element: <NotFound />
-    }
-  ];
   const routes = useRoutes(routesArr);
   return (
     <ErrorBoundary
-      childrenNode={<Suspense fallback={<h2>loading...</h2>}>{routes}</Suspense>}
+      childrenNode={
+        <Suspense fallback={<h2>loading...</h2>}>{routes}</Suspense>
+      }
     />
   );
 }

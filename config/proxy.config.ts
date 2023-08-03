78f28c730014a782dafaae6e32cdb7c69a5561dd
Midolii <462885536@qq.com>
2023-08-03 08:09:36 +0800

    feat: add userAvatar proxy

--- config/proxy.config.ts
+++ config/proxy.config.ts
@@ -1,19 +1,28 @@
+const target = "http://192.168.0.100:3000";
 export const BlhxProxyConfig = {
   "/blhx-api": {
-    target: "http://192.168.0.100:3000",
+    target,
     rewrite: (path: string) => {
       const p = path.replace("/blhx-api", "");
-      console.log(`${path} => http://192.168.0.100${p}`);
+      console.log(`${path} => ${target}${p}`);
       return p;
     },
   },
 };
 export const BaseProxyConfig = {
   "/api1": {
-    target: "http://192.168.0.100:3000",
+    target,
     rewrite: (path: string) => {
       const p = path.replace("/api1", "");
-      console.log(`${path} => http://192.168.0.100${p}`);
+      console.log(`${path} => ${target}${p}`);
+      return p;
+    },
+  },
+  "/userAvatar": {
+    target: "http://192.168.0.100:3000",
+    rewrite: (path: string) => {
+      const p = path;
+      console.log(`${path} => ${target}${p}`);
       return p;
     },
   },
